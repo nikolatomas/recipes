992488ef91c3701cf3af9da4c26def94ea74dd9a
Nikola Tomas <nikola.tomas@gauss.dev>
2022-11-14 09:47:38 +0100

    My first commit

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shoping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription!:Subscription;
+  editMode = false;
+  editedItemIndex!:number;
+  editedItem!: Ingredient;
+  @ViewChild('editRecipeForm') slForm!: NgForm;
+
+  constructor(private shoppingListService: ShoppingListService) { }
+
+  ngOnInit() {
+    setTimeout(()=>{
+      this.subscription = this.shoppingListService.startedEditing.subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+          this.editedItem = this.shoppingListService.getIngredient(index)
+          this.slForm.setValue({
+            'name': this.editedItem.name,
+            'amount': this.editedItem.amount
+          })
+        }
+      );
+    })
+  }
+ngOnDestroy(): void {
+  this.subscription.unsubscribe()
+}
+
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete(){
+    this.shoppingListService.deleteIngredient(this.editedItemIndex)
+    this.onClear()
+  }
+
+  onSubmit(form: NgForm){
+    const name = form.value.name;
+    const amount = form.value.amount;
+    const newIngredient = new Ingredient(name,amount)
+    if(this.editMode){
+      this.shoppingListService.updateIngredient(this.editedItemIndex,newIngredient)
+    } else {
+      this.shoppingListService.addIngredient(newIngredient)
+    }
+    this.editMode = false;
+    form.reset();
+  }
+}
